3ee1cc58272eacf33b544ad000dcfcc2b9a7d055
sajalkhan <sajalhossen@yahoo.com>
2022-11-05 00:22:43 +0600

    list all the movies genres

---
+++ src/api/useGetGenres.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+const fetchMovieData = () => {
+  const API_KEY = 'cd890f94a756b1518a2a17617a5b430e';
+  return axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+};
+
+export const useGetGenres = (onSuccess?: (data: any) => void, onError?: (err: any) => void) => {
+  return useQuery('genre', fetchMovieData, {
+    staleTime: 5000,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess,
+    onError,
+    select: data => data?.data?.genres,
+  });
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,25 @@
+import { useGetGenres } from 'api/useGetGenres';
 import React from 'react';
 
+type Genres = {
+  id: string;
+  name: string;
+};
+
 const Home: React.FC = () => {
+  const { data } = useGetGenres();
+
   return (
     <div className="p-home">
-      <div className="p-home__content">Home Page</div>
+      <div className="p-home__content">
+        {data?.map((item: Genres, indx: number) => {
+          return (
+            <ul key={indx}>
+              <li>{item.name}</li>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 };
