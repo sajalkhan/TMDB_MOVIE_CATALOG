fcdae240fdbd1fbc212cb83a4461730420046dab
sajalkhan <sajalhossen@yahoo.com>
2022-11-05 15:42:16 +0600

    add default imgae

--- src/components/atoms/card/index.tsx
+++ src/components/atoms/card/index.tsx
@@ -1,7 +1,7 @@
+import DefaultImage from 'assets/images/default_image.png';
 import { Text } from 'components/atoms/text';
 import { mapModifiers, ModifierProp } from 'libs/component';
 import React from 'react';
-
 export interface cardProps {
   modifiers?: ModifierProp<'large' | 'medium' | 'small'>;
   className?: string;
@@ -22,11 +22,11 @@ export const Card: React.FC<cardProps> = ({
   const componentClassName = mapModifiers('a-card', modifiers);
   const className = `${componentClassName} ${additionalClassName}`.trim();
 
-  const imgBaseUrl = 'https://image.tmdb.org/t/p/w342';
+  const imgBaseUrl = 'https://image.tmdb.org/t/p/w342' + imgUrl;
 
   return (
     <div className={className} key={id} onClick={onClick}>
-      <img className="a-card__image" src={imgBaseUrl + imgUrl} alt="card img" />
+      <img className="a-card__image" src={imgUrl ? imgBaseUrl : DefaultImage} alt="image" />
       <Text weight="bold">{movieName}</Text>
     </div>
   );
