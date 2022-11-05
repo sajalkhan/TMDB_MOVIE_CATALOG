b2ba41e80f5a8e992b715aafa606e68e1755e5e2
sajalkhan <sajalhossen@yahoo.com>
2022-11-05 19:45:42 +0600

    implement default route

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,17 +1,17 @@
 import { Loading } from 'components/atoms/loading';
 import { ROUTES } from 'constants/routes';
 import React, { lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('pages/home'));
 const Genre = lazy(() => import('pages/genre'));
-// const NotFound = lazy(() => import('pages/not-found'));
 
 export const PagesRoutes: React.FC = () => (
   <Suspense fallback={<Loading overlay />}>
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.MOVIES} element={<Genre />} />
+      <Route path="/" element={<Navigate to={ROUTES.HOME} />} />
     </Routes>
   </Suspense>
 );
