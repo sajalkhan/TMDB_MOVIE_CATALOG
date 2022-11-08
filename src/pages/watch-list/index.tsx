d0e6041894ab57397dddfc0114108ec9dc84033e
sajalkhan <sajalhossen@yahoo.com>
2022-11-08 22:59:50 +0600

    implement remove item functionality

--- src/pages/watch-list/index.tsx
+++ src/pages/watch-list/index.tsx
@@ -1,16 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Heading } from 'components/atoms/heading';
 import { CardList } from 'components/molecules/card-list';
-import { useMoviesState } from 'contexts/movies';
+import { useMoviesDispatch, useMoviesState } from 'contexts/movies';
 import React from 'react';
 
 const Genre: React.FC = () => {
   const watchList = useMoviesState();
+  const dispatch = useMoviesDispatch();
+
+  const handleWatchList = (id: any) => {
+    dispatch({
+      type: 'REMOVE',
+      id,
+    });
+  };
 
   return (
     <div className="p-watch-list">
       <div className="p-watch-list__content">
         <Heading>{watchList.length ? 'Favorite Movie List' : 'No Movie Added'}</Heading>
-        <CardList movies={watchList || []} />
+        <CardList movies={watchList || []} onClick={handleWatchList} />
       </div>
     </div>
   );
