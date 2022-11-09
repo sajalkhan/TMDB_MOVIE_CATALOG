baa9d5cb14e5aadd6de52dd3a3b7c095a0344768
sajalkhan <sajalhossen@yahoo.com>
2022-11-10 00:04:45 +0600

    include add to fav function  from genre page

--- src/pages/genre/index.tsx
+++ src/pages/genre/index.tsx
@@ -1,20 +1,33 @@
 import { UseGetTopMovies } from 'api/useGetTopMovies';
 import { Heading } from 'components/atoms/heading';
 import { CardList } from 'components/molecules/card-list';
+import { useMoviesDispatch } from 'contexts/movies';
 import React from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
 const Genre: React.FC = () => {
   const { id } = useParams();
   const location = useLocation();
+  const dispatch = useMoviesDispatch();
   const { genre } = location.state as never;
   const { data, isLoading } = UseGetTopMovies(id ?? '');
+
+  const handleWatchList = (id: string | number | undefined, imgUrl: string, movieName: string) => {
+    dispatch({
+      type: 'ADD',
+      payload: {
+        id,
+        imgUrl,
+        movieName,
+      },
+    });
+  };
 
   return (
     <div className="p-genre">
       <div className="p-genre__content">
         <Heading>Top 10 Movies in {genre}</Heading>
-        <CardList movies={data || []} isLoading={isLoading} />
+        <CardList movies={data || []} onClick={handleWatchList} isLoading={isLoading} />
       </div>
     </div>
   );
